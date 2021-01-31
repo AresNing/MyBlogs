64dc0234be7691e543a94e4acacd4a7941ae29cf
AresNing <1418226860@qq.com>
2021-01-31 13:46:27 +0800

    commit

--- docs/plugin/twemoji.js
+++
@@ -1,8 +0,0 @@
-(function () {
-    function install(hook, vm) {
-        hook.doneEach(function () {
-            twemoji.parse(document);
-        });
-    }
-    $docsify.plugins = [].concat(install, $docsify.plugins);
-})();
